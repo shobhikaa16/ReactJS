fdb25d9bc52f18155ebde7ac5147bced77010d96
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-01-06 10:39:44 +0000

    react four

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Chai from "./Shubhi"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Chai/>
+    <h2>Learing the React with chai aur code </h2>
+    <p>Paragraph tag is used to write para</p>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Shubhi.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>Learing the React </h3>
+    )
+}
+export default Chai
