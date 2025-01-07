a4e308c4f42ff8d482e5302d0068c730142f61e5
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-01-07 09:04:45 +0000

    react day 4

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import Chai from "./Shubhi"
 
 function App() {
+    const username = "chai aur code"
  
 
   return (
     <>
     <Chai/>
-    <h2>Learing the React with chai aur code </h2>
+    <h2>Learing the React with chai aur code ans this is a variable-: {username}</h2>
     <p>Paragraph tag is used to write para</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+
+
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// };
+
+const anotheruser = "chai aur code"
+const ReactElement =React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},
+  'click me to visit google',
+  anotheruser
+)
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+);
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+   //<App />
+    ReactElement
+    //anotherElement
+    // or we can do the same with the given below as MyApp is function and we call it but hum aese likhenge nhi
+    // MyApp()
+
 )
