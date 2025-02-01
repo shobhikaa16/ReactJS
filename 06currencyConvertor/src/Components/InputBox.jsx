059daab7eabb9e11d33609635aa904e24d83deba
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-01 14:06:32 +0000

    project done

--- 06currencyConvertor/src/Components/InputBox.jsx
+++ 06currencyConvertor/src/Components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useId} from 'react'
 
 
 function InputBox({   // this inputbox is taking two inputs from you label and classname
@@ -13,17 +13,19 @@ function InputBox({   // this inputbox is taking two inputs from you label and c
     
     className = "",
 }) {
+    const amountInputId = useId()
    
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}
+                 className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
 
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -36,12 +38,18 @@ function InputBox({   // this inputbox is taking two inputs from you label and c
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    // this is the select field where we have to loop for the values
+                    value = {selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled = {currencyDisable}
                 >
                     
-                        <option value="usd">
-                            usd
-                        </option>
+                        {currencyOptions.map((currency) =>( //to use loops remember key
+                            <option key={currency} 
+                            value={currency}> 
+                            {currency}
+                            </option>
+                        ))}
                 
                 </select>
             </div>
